032088d832368e2f4abd942f33e9556e8a5f1b0c
n-kanou <n-kanou@y-i-group.co.jp>
2021-08-17 19:03:31 +0900

    １－３
    課題提出

---
+++ JavaStudy/JavaScript/1-3/main.js
@@ -0,0 +1,24 @@
+let a = 15;
+if(a  === 10){
+    console.log(a);
+}
+
+let b = 9;
+if (b >=10 && b <20 && b % 2 === 0);
+else {
+console.log(b);
+}
+let x =10;
+if (x >= 10 && x <= 20) {
+    console.log("成功です");
+}else  {
+    console.log('不合格');
+}
+
+let y =0;
+if (y % 2 === 0) {
+    console.log("偶数です");
+}
+if (y % 2 !== 0) {
+    console.log("奇数です");
+}
